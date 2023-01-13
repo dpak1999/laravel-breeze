99b9d111ffada92a2e70e60ed5e3496bfbbbf988
Deepak <deepakkumardash313@gmail.com>
2023-01-13 23:52:31 +0530

    add : controller for routes

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,13 @@
 <?php
 
+use App\Http\Controllers\Demo\DemoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get("/about", function () {
-    return view("about");
-});
-
-Route::get("/contact", function () {
-    return view("contact");
+Route::controller(DemoController::class)->group(function () {
+    Route::get("/about", "Index");
+    Route::get("/contact", "ContactMethod");
 });
